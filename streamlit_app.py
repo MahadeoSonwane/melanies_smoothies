316793ccabbb68b15675ba9ee3cae60cc57363f1
MahadeoSonwane <mahadeosonwane82@gmail.com>
2025-09-09 14:31:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,44 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title('🥤 Customize Your Smoothie! 🥤')
-st.write(
-    "Choose the fruits you want in your custom Smoothie!"
-)
+st.write("Choose the fruits you want in your custom Smoothie!")
 
-import streamlit as st
-
+# Input for smoothie name
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
+# Connect to Snowflake (depending on your setup)
+# Option 1: Use get_active_session if available
+session = get_active_session()
+
+# Option 2: Use st.connection if your Streamlit supports it
+# cnx = st.connection("snowflake")
+# session = cnx.session()
+
+# Load data from Snowflake
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 st.dataframe(my_dataframe, use_container_width=True)
 
+# Multiselect for ingredients
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
-    my_dataframe,
+    my_dataframe['FRUIT_NAME'].to_list(),  # pass a list of strings here
     max_selections=5
 )
 
 if ingredients_list:
-    ingredients_string = ''
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+    ingredients_string = ' '.join(ingredients_list)
     st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-    values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+    my_insert_stmt = f"""
+    INSERT INTO smoothies.public.orders(ingredients, name_on_order)
+    VALUES ('{ingredients_string}', '{name_on_order}')
+    """
     
-st.write(my_insert_stmt)
+    st.write(my_insert_stmt)
+
 st.stop()
